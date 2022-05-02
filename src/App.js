b89fbffb84cae44748ada7ddb05fe55b65356104
nbrennan26 <nbrennan26@gmail.com>
2022-05-02 15:34:44 -0500

    Add basic component elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
+import Welcome from "./components/Welcome"
+
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Header />
+      <Welcome />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
