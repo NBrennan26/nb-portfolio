b1cc86b0c4ba9d4347b0a6927bbb5b9fa932b157
nbrennan26 <nbrennan26@gmail.com>
2022-05-04 14:06:10 -0500

    Basic project tamplate layout set up

--- src/components/ProjectTile.jsx
+++ src/components/ProjectTile.jsx
@@ -1,11 +1,21 @@
 function ProjectTile({ project }) {
   return (
     <div className="project-tile">
-      <p>{project.title}</p>
-      <p>{project.description}</p>
-      <p>{project.link}</p>
-      <p>{project.github}</p>
-      <p>{project.photo}</p>
+      <div className="project-img-cont">{project.photo}</div>
+      <div className="project-data-cont">
+        <div className="project-title">{project.title}</div>
+        <div className="project-desc">{project.description}</div>
+        <div className="project-link">
+          <a href={project.link} target="_blank" rel="noreferrer">
+            Preview
+          </a>
+        </div>
+        <div className="project-github">
+          <a href={project.github} target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
