3e11e2842c9ae893ff6b6b3d58ab75c2a58d767f
nbrennan26 <nbrennan26@gmail.com>
2022-05-04 15:01:49 -0500

    Further refinement of project tile styling

--- src/components/ProjectTile.jsx
+++ src/components/ProjectTile.jsx
@@ -1,22 +1,27 @@
 function ProjectTile({ project }) {
-
-  const projectTitle = project.title
+  const projectTitle = project.title;
 
   return (
     <div className="project-tile">
-      <div className="project-img-cont"><img src={project.photo} alt={projectTitle} className="project-img" /></div>
+      <div className="project-img-cont">
+        <img src={project.photo} alt={projectTitle} className="project-img" />
+      </div>
       <div className="project-data-cont">
-        <div className="project-title">{project.title}</div>
-        <div className="project-desc">{project.description}</div>
-        <div className="project-link">
-          <a href={project.link} target="_blank" rel="noreferrer">
-            Preview
-          </a>
+        <div className="project-desc-cont">
+          <div className="project-title">{project.title}</div>
+          <div className="project-desc">{project.description}</div>
         </div>
-        <div className="project-github">
-          <a href={project.github} target="_blank" rel="noreferrer">
-            GitHub
-          </a>
+        <div className="project-links-cont">
+          <div className="project-link">
+            <a href={project.link} target="_blank" rel="noreferrer">
+              Preview
+            </a>
+          </div>
+          <div className="project-github">
+            <a href={project.github} target="_blank" rel="noreferrer">
+              GitHub
+            </a>
+          </div>
         </div>
       </div>
     </div>
