cf7c63dece15e7e3848b36f026e39221ea521e8d
nbrennan26 <nbrennan26@gmail.com>
2022-05-04 15:31:26 -0500

    a few more general style updates. Got rid of border boxes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,11 +3,16 @@ import { projects } from "../features/projects/projectData";
 
 function Projects() {
   return (
-    <section className="projects-cont" id="projects">
-      {projects.map((project) => (
-        <ProjectTile key={project.id} project={project} />
-      ))}
-    </section>
+    <>
+      <div className="section-title">
+        <span>Projects</span>
+      </div>{" "}
+      <section className="projects-cont" id="projects">
+        {projects.map((project) => (
+          <ProjectTile key={project.id} project={project} />
+        ))}
+      </section>
+    </>
   );
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,32 +7,37 @@ import {
 
 function Skills() {
   return (
-    <section className="skills-cont" id="skills">
-      <div className="skill-block">
-        <span className="skill-block-title">Front-End Skills</span>
-        <div className="skill-block-cont">
-          {frontendSkills.map((skill) => (
-            <SkillTile key={skill.id} skill={skill} />
-          ))}
+    <>
+    <div className="section-title">
+      <span>Skills</span>
+    </div>
+      <section className="skills-cont" id="skills">
+        <div className="skill-block">
+          <span className="skill-block-title">Front-End Skills</span>
+          <div className="skill-block-cont">
+            {frontendSkills.map((skill) => (
+              <SkillTile key={skill.id} skill={skill} />
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="skill-block">
-        <span className="skill-block-title">Back-End Skills</span>
-        <div className="skill-block-cont">
-          {backendSkills.map((skill) => (
-            <SkillTile key={skill.id} skill={skill} />
-          ))}
+        <div className="skill-block">
+          <span className="skill-block-title">Back-End Skills</span>
+          <div className="skill-block-cont">
+            {backendSkills.map((skill) => (
+              <SkillTile key={skill.id} skill={skill} />
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="skill-block">
-        <span className="skill-block-title">Other Skills</span>
-        <div className="skill-block-cont">
-          {otherSkills.map((skill) => (
-            <SkillTile key={skill.id} skill={skill} />
-          ))}
+        <div className="skill-block">
+          <span className="skill-block-title">Other Skills</span>
+          <div className="skill-block-cont">
+            {otherSkills.map((skill) => (
+              <SkillTile key={skill.id} skill={skill} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
