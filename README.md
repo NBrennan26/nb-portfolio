0531c6ee77d940d08bc8e64c0adc7ec830dfc7e8
nbrennan26 <nbrennan26@gmail.com>
2022-05-23 13:51:18 -0500

    Update about section and README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-To Do:
-Create README
+# Nick Brennan Personal Portfolio
+
+## Built With
+
+- HTML
+- CSS
+- Javascript/React
+
+  ## To Do
+
+- Consider adding animations/background transitions
+- Maintain Skills/Projects sections
+
+## Acknowledgments
+
+- **Billie Thompson** - _Provided README Template_ -
+  [PurpleBooth](https://github.com/PurpleBooth)
+
+- **The Odin Project** - _General Guidance & Project inspiration_ - [TheOdinProject](https://www.theodinproject.com/)
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,37 +1,27 @@
 function About() {
   return (
     <section className="about-cont" id="about">
-      <div>
-        <span>Here's a little about me: </span>
-        <br />
-        <span>Originally from Chicago, I moved to Austin in 2021.</span>
-        <br />
-        <span>
-          Since then I have been studying Web Development while continuing to
-          serve as a Field Artillery Captain in the Army National Guard.
-        </span>
-        <br />
-        <span>
-          In the past I've worked some interesting jobs including bike courier,
-          private security, bartender, real estate agent, and pharmacy tech.
-        </span>
+      <div className="about-section">
+        <div className="about-section-title">Here's a little about me: </div>
+        <p>
+          Originally from Chicago, I moved to Austin in 2021. Since then, I have
+          been studying Web Development full time while continuing to serve as a
+          Field Artillery Captain in the Army National Guard. In the past, I've
+          worked some interesting jobs including bike courier, private security,
+          bartender, real estate agent, and pharmacy tech.
+        </p>
       </div>
-      <div className="about-fun-facts">
-        <span>Some Fun Facts: </span>
-        <br />
-        <span>
-          In College, I taught chess to kids in an after school program.
-        </span>
-        <br />
-        <span>
-          I once went on a week long bike/camping trip around Lake Michigan.
-        </span>
-        <br />
-        <span>
+      <ul className="about-section">
+        <div className="about-section-title">Some Fun Facts: </div>
+        <li>
+          In College, I taught chess through an elementary school program.
+        </li>
+        <li>I once went on a 300 mile bike trip around Lake Michigan.</li>
+        <li>
           I've lived in four states, been to 41 states, and travelled to 14
           countries.
-        </span>
-      </div>
+        </li>
+      </ul>
     </section>
   );
 }
